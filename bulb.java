110cef161c3adc96866600866fa8aacd1f3e1846
Suraj Chaudhary <ac624486@gmail.com>
2022-08-21 23:32:18 +0530

    Bulb commit

---
+++ bulb.java
@@ -0,0 +1,11 @@
+package LetsLearnGit;
+
+public class bulb {
+    public static void main(String[] args) {
+        String s="Bulb";
+        int n=s.length();
+        System.out.println(n);
+
+    }
+    
+}
